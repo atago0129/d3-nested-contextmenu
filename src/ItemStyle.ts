7ad228ec55164a646aa46aca860352e0f006c2dd
Atsushi Tago <atago0129@users.noreply.github.com>
2018-12-10 22:01:10 +0900

    update ItemStyle.ts

--- src/ItemStyle.ts
+++ src/ItemStyle.ts
@@ -1,5 +1,32 @@
 export class ItemStyle {
-  constructor(private _fontSize: number){
+  private _fontSize = 11;
+  
+  private _fillColor = 'rgb(250, 250, 250)';
+  
+  private _onMouseFillColor = 'rgb(200, 200, 200)';
+  
+  constructor(fontSize: number|null, fillColor: string|null, onMouseFillColor: string|null){
+    if (fontSize !== null) {
+      this._fontSize = fontSize;
+    }
+    if (fillColor !== null) {
+      this._fillColor = fillColor;
+    }
+    if (onMouseFillColor !== null) {
+      this._onMouseFillColor = onMouseFillColor;
+    }
+  }
+  
+  get fontSize(): number {
+    return this._fontSize;
+  }
+  
+  get fillColor(): string {
+    return this._fillColor;
+  }
+  
+  get onMouseFillColor(): string {
+    return this._onMouseFillColor;
   }
 
   public getFontSize(): number {
